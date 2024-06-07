3e5019c33fdd587c24886724ecf17a8937d30629
SaudMuhammadAli <saud.al.faisal927@gmail.com>
2024-06-07 14:34:20 +0500

    arithmetic operators Covered

---
+++ main.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//Arithmetic Operators
+var x = 10;
+var y = 5;
+var sum = --x;
+console.log(sum);
---
+++ main.ts
@@ -0,0 +1,6 @@
+//Arithmetic Operators
+let x = 10;
+let y = 5;
+let sum = --x;
+console.log(sum);
+export {};
